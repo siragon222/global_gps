1527e36f13db38998598601ed96dd5823db917a9
Ronald <siragon222@gmail.com>
2025-08-16 20:18:59 -0400

    Updates

--- send_email.php
+++ send_email.php
@@ -26,7 +26,7 @@ $email = $data['email'];
 $phone = $data['phone'];
 
 // Dirección de correo a la que se enviará el mensaje
-$to_email = 'siragon222@gmail.com';
+$to_email = 'subcripcion@globalgps.com';
 
 // Configurar el correo
 $subject = 'Nuevo mensaje de suscripción desde la web';
